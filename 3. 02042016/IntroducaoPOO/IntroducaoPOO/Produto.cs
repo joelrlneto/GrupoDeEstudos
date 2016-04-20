84a279654b3d8132e7c3a4900978a8aaf4456e84
Joel Rodrigues <joelrlneto@gmail.com>
2016-04-20 08:44:45 -0300

    Material da aula do dia 2/4/2016

---
+++ 3. 02042016/IntroducaoPOO/IntroducaoPOO/Produto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroducaoPOO
+{
+    //A classe deve ser pública para que possa ser acessada externamente
+    public class Produto
+    {
+        //Atributos: privados
+        private string codigo;
+        private string descricao;
+        private double precoVenda;
+        private double precoCusto;
+
+        //Propriedades: públicas. Encapsulam os atributos, controlando o acesso externo a eles.
+        public string Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        public string Descricao
+        {
+            get
+            {
+                return descricao;
+            }
+            set
+            {
+                // Validando a entrada de dados: a descrição não pode ser vazia
+                // Para quem utiliza essa classe, essa validação é transparente, ou seja, pode-se atribuir qualquer valor e a classe se encarrega de validar
+                if (value == "")
+                    descricao = "[Produto sem descrição]";
+                else
+                    descricao = value;
+            }
+        }
+
+        public double PrecoVenda
+        {
+            get
+            {
+                return precoVenda;
+            }
+            set
+            {
+                // Validando a entrada de dados: o preço não pode ser negativo
+                // Para quem utiliza essa classe, essa validação é transparente, ou seja, pode-se atribuir qualquer valor e a classe se encarrega de validar
+                if (value < 0)
+                    precoVenda = 0;
+                else
+                    precoVenda = value;
+            }
+        }
+
+        //Construtor padrão, usado para iniciar os valores dos atributos
+        public Produto()
+        {
+            codigo = "000";
+            descricao = "[Produto sem descrição]";
+            precoVenda = 0;
+        }
+
+        //Construtor com argumentos, usado para iniciar os valores dos atributos a partir de parâmetros
+        public Produto(string _codigo, string _descricao, double _precoVenda)
+        {
+            codigo = _codigo;
+            descricao = _descricao;
+            precoVenda = _precoVenda;
+        }
+
+        //Método com retorno, mas sem argumentos
+        public string GetDetalhes()
+        {
+            return $"Codigo: {codigo} | Descrição: {descricao} | Preço: R$ {precoVenda}";
+        }
+
+        //Método sem retorno, mas com argumento
+        public void AplicarDesconto(double percentual)
+        {
+            precoVenda = precoVenda * (1 - (percentual/100));
+        }
+    }
+}
---
+++ 3. 02042016/IntroducaoPOO/IntroducaoPOO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroducaoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo de uso 1: Usando o construtor padrão
+
+            Produto p1 = new Produto();
+
+            Console.WriteLine(p1.GetDetalhes());
+            Console.WriteLine();
+            //-----------------------------------------------------------
+
+            //Exemplo de uso 2: Usando o construtor com argumentos
+            Produto p2 = new Produto("111", "Notebook Dell i5 8GB 1TB", 2400);
+
+            Console.WriteLine(p2.GetDetalhes());
+            Console.WriteLine();
+            //-----------------------------------------------------------
+
+            //Exemplo de uso 3: Usando Object Initializer
+            Produto p3 = new Produto
+            {
+                Codigo = "222",
+                Descricao = "Adaptador HDMI/VGA Multilaser",
+                PrecoVenda = 68.99
+            };
+
+            Console.WriteLine(p3.GetDetalhes());
+            Console.WriteLine();
+            //-----------------------------------------------------------
+
+            //Exemplo de uso 4: Usando métodos da classe (considerando o objeto p3 criado acima)
+            p3.AplicarDesconto(10);
+
+            Console.WriteLine(p3.GetDetalhes());
+            Console.WriteLine();
+            //-----------------------------------------------------------
+
+            //Exemplo de uso 5: Atribuindo um valor inválido ao preço do objeto p2 (será validado no encapsulamento do atributo)
+            p2.PrecoVenda = -1;
+            p2.Descricao = "";
+            Console.WriteLine(p2.GetDetalhes());
+            Console.WriteLine();
+        }
+    }
+}
